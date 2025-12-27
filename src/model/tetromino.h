d1a28579372f06f9a40b9f404b36e7a72200fb90
siphux <adamb@DESKTOP-4VL4UV8.localdomain>
2025-12-27 14:32:22 +0400

    Tetromino structure finished

--- src/model/tetromino.h
+++ src/model/tetromino.h
@@ -2,13 +2,63 @@
 #include <vector>
 #include "piece_rotation.h"
 
+class Point{
+    public:
+    int x;
+    int y;
+
+    Point(int X, int Y): x(X), y(Y) {}
+}
+
+enum TetrominoType { I, J, L, O, S, T, Z};
+
+
 class Tetromino {
-public:
-    Tetromino(const std::vector<PieceRotation>& r);
+    private:
+        TetrominoType type;    
+        int rotation_state;
+        Point board_position;
+        std::vector<Point> blocks;
+    
+    public:
+        Tetromino(TetrominoType t){
+            type = t;
+            rotation_state = 0;
+            board_position = Point(4,0);
+            switch(t){
+                case I: colorId = 1; blocks = {{-1, 0}, {0, 0}, {1, 0}, {2, 0}}; break; // Cyan
+                case J: colorId = 2; blocks = {{-1,-1}, {-1,0}, {0, 0}, {1, 0}}; break; // Blue
+                case L: colorId = 3; blocks = {{1, -1}, {-1,0}, {0, 0}, {1, 0}}; break; // Orange
+                case O: colorId = 4; blocks = {{0,  0}, {1, 0}, {0, 1}, {1, 1}}; break; // Yellow
+                case S: colorId = 5; blocks = {{0,  0}, {1, 0}, {-1,1}, {0, 1}}; break; // Green
+                case T: colorId = 6; blocks = {{-1, 0}, {0, 0}, {1, 0}, {0, 1}}; break; // Purple
+                case Z: colorId = 7; blocks = {{-1, 0}, {0, 0}, {0, 1}, {1, 1}}; break; // Red
+            }
+        }
 
-    const PieceRotation& getRotation(int index) const;
-    int rotationCount() const;
+        TetrominoType getType() const {
+            return type;
+        }
+        Point getPosition() const {
+            return board_position;
+        }
+        void setPosition(int x, int y) {
+            board_position.x = x;
+            board_position.y = y;
+        }
+        std::vector<Point> getBlocks() const {
+            return blocks;
+        }
+        int getRotationState() const {
+            return rotation_state;
+        }
 
-private:
-    std::vector<PieceRotation> rotations;
+        void rotateAntiClockwise(){
+            rotation_state = (rotation_state + 1)%4;
+            for (auto& block : blocks){
+                int temp = block.x;
+                block.x = block.y;
+                block.y = -temp;
+            }
+        }
 };
